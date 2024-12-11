e448d0587e47bff5569e987d85d3ee488b7945d4
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2024-12-11 22:54:30 +0800

    "data as of" indicator included in custodian dashboard

--- views/custodian/create.view.php
+++ views/custodian/create.view.php
@@ -9,6 +9,9 @@ require base_path('views/partials/head.php') ?>
       <?php require base_path('views/partials/banner.php') ?>
       <div class="school-name-container">
             <h2 class="school-name"><?= $schoolName ?></h2>
+            <h2 class="date">
+               <?php echo "Data as of " . date("F Y"); ?>
+            </h2>
       </div>    
       <!-- Hide add receipt temporarily -->
       <!-- <//?php require base_path('views/partials/custodian/add_receipt_modal.php') ?> -->
